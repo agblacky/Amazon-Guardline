443ac7df2bbe75dea56d7cf74fe39d190afbeed4
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2020-10-17 17:35:49 +0200

    Enemy Update
    + Enemy Class
    + Enemy Spawner
    + Human Prefab

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Human
+{
+    public int spawnTime;
+    public HumanType humanType;
+}
+public enum HumanType
+{
+    Human_Basic,
+    Human_Placeholder,
+    Human_Placeholder2,
+}
---
+++ Assets/Scripts/HumanSpawner.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanSpawner : MonoBehaviour
+{
+    public List<Human> humans;
+}
