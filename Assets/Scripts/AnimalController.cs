4b4dbd4d6629a7be49d5f16752db41eed2b1ef6e
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2020-11-11 09:28:04 +0100

    Logic Update

--- Assets/Scripts/AnimalController.cs
+++ Assets/Scripts/AnimalController.cs
@@ -50,6 +50,8 @@ public class AnimalController : MonoBehaviour
                 //Wall Code
             }
 
+
         }
+        
     }
 }
--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,18 +8,42 @@ public class FrogController : MonoBehaviour
     public float attackCooldown;
     private float attackTime;
     public int damageValue;
+    private bool isAttacking;
+    private List<Collider2D> collisions;
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 11)
         {
+            this.collisions.Add(collision);
+            isAttacking = true; //fa´lss das nicht geht, in eine Liste setzen, welche angegriffen werden
+        }
+            
+    }
+
+    private void Update()
+    {
+        if (isAttacking)
+        {
+
             if (attackTime <= Time.time)
             {
-                collision.gameObject.GetComponent<HumanController>().ReceiveDamage(damageValue);
+                foreach(Collider2D collision in collisions)
+                {
+                    collision.gameObject.GetComponent<HumanController>().ReceiveDamage(damageValue);
+                    
+                }
                 //Cooldown
                 attackTime = Time.time + attackCooldown;
             }
         }
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collisions.Remove(collision);
+        isAttacking = false;
+    }
 
 }
