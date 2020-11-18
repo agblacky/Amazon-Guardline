4e75cf5769fcfce7970aedff7605c724f1251218
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2020-11-18 17:01:23 +0100

    Updated Shop

--- Assets/Scripts/Foodscript.cs
+++ Assets/Scripts/Foodscript.cs
@@ -1,13 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Foodscript : MonoBehaviour
 {
-    
-    public void OnPointerClick(PointerEventData eventData)
+    public Text text;
+    private void Start()
     {
-        
+        text = GameObject.Find("FoodcounterText").GetComponent<Text>();
+        Instantiate(text, transform);
+        Button btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(Add);
     }
+
+    public void Add()
+    {
+        text.text = Convert.ToString(Convert.ToInt16(text.text) + 50);
+        Destroy(this.gameObject);
+    }
+    
 }
