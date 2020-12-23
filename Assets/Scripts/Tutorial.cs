5ec0b67a57cefc2ba49c1bbd5006a35356ad3fd3
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2020-12-23 13:23:46 +0100

    Tutorial Added

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject canvas;
+    public List<Image> images;
+    private int counter=0;
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("TutorialDone"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+        else
+        {
+            canvas = this.gameObject;
+            canvas.GetComponent<Image>().color = new Color(256, 256, 256);
+            GameObject.Find("Button").transform.SetParent(canvas.transform);
+            Next();
+
+        }
+        
+    }
+    public void Next()
+    {
+        if (counter == images.Count - 1)
+        {
+            PlayerPrefs.SetInt("TutorialDone", 1);
+            SceneManager.LoadScene("MainMenu");
+        }
+        else
+        {
+            canvas.GetComponent<Image>().sprite = images[counter].GetComponent<Image>().sprite;
+            counter++;
+        }
+    }
+}
